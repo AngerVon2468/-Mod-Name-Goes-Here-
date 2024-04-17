3d2294bc07d0b2a59de8b65ef969bce803b71d13
AngerVon2468 <thomaschad0909@gmail.com>
2024-04-17 15:14:10 +1000

    Inital Commit

---
+++ src/main/java/net/mavity/mod_name_goes_here/ModNameGoesHereDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mavity.mod_name_goes_here;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModNameGoesHereDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
